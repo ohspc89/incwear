46870175860ce1e7351dab346965057f53bd1d29
Jinseok Oh <jinseok.oh@pm.me>
2025-12-01 20:23:13 -0800

    Requirements added

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ import setuptools
 with open("README.md") as file:
     read_me_description = file.read()
 
-setuptools.setup(name="incwear",
-        version="0.1",
-        author="Jinseok Oh, PhD",
-        author_email="jinseok.oh@pm.me",
-        description="A package to analyze wearable sensor data",
-        long_description=read_me_description,
-        long_description_content_type="text/markdown",
-        url= "github.com/ohspc89/incwear.git",
-        packages=['incwear'],
-        classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License", "Operating System :: OS Independent",],
-        python_requires='>=3.10',)
+setuptools.setup(
+    name="incwear",
+    version="0.1",
+    author="Jinseok Oh, PhD",
+    author_email="jinseok.oh@pm.me",
+    description="A package to analyze wearable sensor data",
+    long_description=read_me_description,
+    long_description_content_type="text/markdown",
+    url= "github.com/ohspc89/incwear.git",
+    packages=['incwear'],
+    classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License", "Operating System :: OS Independent",],
+    python_requires='>=3.10',
+    install_requires=['numpy', 'pandas', 'scipy', 'pytz', 'pyarrow', 'scikit-digital-health', 'matplotlib', 'EntropyHub==2.0'])
